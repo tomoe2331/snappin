d15fe77c0aca9f681aaf89e252a89ab3dd2393d9
tomoe2331 <randomgamer674@gmail.com>
2024-10-13 21:29:28 +0800

    Start

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,8 +5,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from './../components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -23,7 +22,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
